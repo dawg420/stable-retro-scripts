f9d42d2fec345b16696f084a8727058627d9c3ab
yuchen <88711624+dawg420@users.noreply.github.com>
2024-04-14 15:56:02 +0800

    Update wwf_trainer.py
    edit train-test sets

--- wwf_trainer.py
+++ wwf_trainer.py
@@ -69,24 +69,14 @@ game_states_veryhard = [
     'VeryHard_Yokozuna-07'
 ]
 
-train_states = [
-    'VeryEasy_Yokozuna-01',
-    'VeryEasy_Yokozuna-02',
-    'VeryEasy_Yokozuna-03',
-    'VeryEasy_Yokozuna-04',
-    'VeryEasy_Yokozuna-05',
-    'VeryHard_Yokozuna-01',
-    'VeryHard_Yokozuna-02',
-    'VeryHard_Yokozuna-03',
-    'VeryHard_Yokozuna-04',
-    'VeryHard_Yokozuna-05',
-]
-
+train_states = [ 
+    'VeryEasy_Yokozuna-03', 
+    'VeryHard_Yokozuna-03', 
+] 
+ 
 test_states = [
-    'VeryHard_Yokozuna-06',
-    'VeryHard_Yokozuna-07',
-    'VeryHard_Yokozuna-06',
-    'VeryHard_Yokozuna-07'
+    'VeryEasy_Yokozuna-04', 
+    'VeryHard_Yokozuna-02', 
 ]
 
 def test_model(args, num_matchs, logger):
